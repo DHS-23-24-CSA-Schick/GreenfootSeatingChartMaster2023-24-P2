a092ffb101cd238f1188a4af9cba2d58545413d9
Megan <iimeganzhang@gmail.com>
2023-09-06 09:37:28 -0700

    Adding my files

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        MeganZhang meganzhang = new MeganZhang();
+        addObject(meganzhang,9,4);
+        meganzhang.assignSeat();
   
     }
     
